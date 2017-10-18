d0b8091f683974bf1825f89bc3caa102af6d921f
Nicky van Urk <nickyvanurk@gmx.com>
2017-10-18 23:06:21 +0200

    Add client entity visualization

--- public/js/main.js
+++ public/js/main.js
@@ -1,8 +1,19 @@
 class Entity {
-  constructor() {
+  constructor(scene) {
     this.x = 0;
     this.y = 0;
     this.z = 0;
+
+    this.mesh = new THREE.Mesh(
+      new THREE.BoxGeometry(1, 1, 1),
+      new THREE.MeshLambertMaterial({color: 0xff0000})
+    );
+    this.mesh.position.set(this.x, this.y, this.z);
+    scene.add(this.mesh);
+  }
+
+  setPosition(position) {
+    this.mesh.position.set(position.x, position.y, position.z);
   }
 }
 
@@ -15,10 +26,43 @@ class Client {
     this.id = null;
 
     this.entities = {};
+
+    this.setUpdateRate(60);
   }
 
   onConnection() {
     console.log('Connected to server');
+
+    this.renderer = new THREE.WebGLRenderer();
+    this.renderer.setSize(window.innerWidth, window.innerHeight);
+    document.getElementById('container').appendChild(this.renderer.domElement);
+
+    this.scene = new THREE.Scene();
+
+    this.camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 10000);
+    this.camera.position.z = 50;
+
+    let light = new THREE.PointLight(0xFFFFFF);
+    light.position.set(10, 0, 10);
+    this.scene.add(light);
+    this.scene.add(new THREE.HemisphereLight());
+  }
+
+  setUpdateRate(hz) {
+    this.updateRate = hz;
+
+    clearInterval(this.updateInterval);
+    this.updateInterval = setInterval(this.update.bind(this), 1000 / this.updateRate);
+  }
+
+  update() {
+    if (this.id == null) return;
+
+    this.render();
+  }
+
+  render() {
+    this.renderer.render(this.scene, this.camera);
   }
 
   processServerMessages(event) {
@@ -35,16 +79,14 @@ class Client {
 
           // if this is the first time we see this entity, create local representation
           if (!this.entities[state.id]) {
-            let entity = new Entity();
+            let entity = new Entity(this.scene);
             entity.id = state.id;
             this.entities[state.id] = entity;
           }
 
           let entity = this.entities[state.id];
 
-          entity.x = state.position.x;
-          entity.y = state.position.y;
-          entity.z = state.position.z;
+          entity.setPosition(state.position);
         }
         break;
     }
