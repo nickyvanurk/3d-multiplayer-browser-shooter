85cce1a2e7bc602ac0d7062821cf015e54df9938
Nicky van Urk <nickyvanurk@gmx.com>
2017-10-18 22:23:17 +0200

    Handle received world state

--- public/js/main.js
+++ public/js/main.js
@@ -1,10 +1,20 @@
+class Entity {
+  constructor() {
+    this.x = 0;
+    this.y = 0;
+    this.z = 0;
+  }
+}
+
 class Client {
   constructor() {
     this.ws = new WebSocket('ws://localhost:8080');
-    this.ws.onopen = this.onConnection;
-    this.ws.onmessage = this.processServerMessages;
+    this.ws.onopen = this.onConnection.bind(this);
+    this.ws.onmessage = this.processServerMessages.bind(this);
 
     this.id = null;
+
+    this.entities = {};
   }
 
   onConnection() {
@@ -20,7 +30,22 @@ class Client {
         console.log(`Client ID set to: ${this.id}`);
         break;
       case 'worldState':
-        console.log(message.states);
+        for (let i = 0; i < message.states.length; i++) {
+          let state = message.states[i];
+
+          // if this is the first time we see this entity, create local representation
+          if (!this.entities[state.id]) {
+            let entity = new Entity();
+            entity.id = state.id;
+            this.entities[state.id] = entity;
+          }
+
+          let entity = this.entities[state.id];
+
+          entity.x = state.position.x;
+          entity.y = state.position.y;
+          entity.z = state.position.z;
+        }
         break;
     }
   }
