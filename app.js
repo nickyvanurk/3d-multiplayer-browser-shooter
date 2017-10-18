c2e9d728abbdfd3d456c2fa6f8a5283ab96f8822
Nicky van Urk <nickyvanurk@gmx.com>
2017-10-18 18:25:46 +0200

    Create server class

--- app.js
+++ app.js
@@ -1,27 +1,33 @@
 const express = require('express');
 const http = require('http');
-const url = require('url');
 const WebSocket = require('ws');
 
 const app = express();
 
 app.use(express.static('public'));
 
-const server = http.createServer(app);
-const wss = new WebSocket.Server({server});
+class Server {
+  constructor(app) {
+    this.httpServer = http.createServer(app);
+    this.wss = new WebSocket.Server({server: this.httpServer});
 
-wss.on('connection', (ws, req) => {
-  ws.on('message', (message) => {
-    console.log('received: %s', message);
-  });
+    this.wss.on('connection', this.onConnection.bind(this));
+  }
 
-  ws.on('close', () => {
-    console.log('client disconnected');
-  });
+  onConnection(client) {
+    client.entityId = this.wss.clients.size - 1;
 
-  ws.send('something');
-});
+    client.on('close', () => {
+      console.log('client disconnected');
+    });
+  }
 
-server.listen(8080, () => {
-  console.log('listening on %d', server.address().port);
-});
+  listen(port) {
+    this.httpServer.listen(port, () => {
+      console.log('listening on %d', this.httpServer.address().port);
+    });
+  }
+}
+
+const server = new Server(app);
+server.listen(8080);
