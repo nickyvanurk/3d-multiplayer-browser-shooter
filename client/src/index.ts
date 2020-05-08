6bde3daa14c56b2be73bd48c2f281965de16784f
Nicky van Urk <nickyvanurk@gmx.com>
2020-05-08 15:17:20 +0200

    Add css support

--- client/src/index.ts
+++ client/src/index.ts
@@ -1,1 +1,3 @@
+import './style.css';
+
 console.log('Hello from clien!t');
--- client/webpack.common.js
+++ client/webpack.common.js
@@ -12,7 +12,14 @@ module.exports = {
         test: /\.tsx?$/,
         use: 'ts-loader',
         exclude: /node_modules/,
-      }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+        ],
+      },
     ]
   },
   plugins: [
