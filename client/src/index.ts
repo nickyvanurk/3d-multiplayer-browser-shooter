371b7825fcf8d79d4352889d8308fe26c69c6e17
Nicky van Urk <nickyvanurk@gmx.com>
2020-05-09 21:10:23 +0200

    Setup basic client/server communication

--- client/src/index.ts
+++ client/src/index.ts
@@ -1,86 +1,107 @@
 import './style.css';
 
-import {World} from 'ecsy';
+// import {World} from 'ecsy';
 
-import Velocity from './components/velocity';
-import Position from './components/position';
-import Shape from './components/shape';
-import Renderable from './components/renderable';
+// import Velocity from './components/velocity';
+// import Position from './components/position';
+// import Shape from './components/shape';
+// import Renderable from './components/renderable';
 
-import Moveable from './systems/moveable';
-import Renderer from './systems/renderer';
+// import Moveable from './systems/moveable';
+// import Renderer from './systems/renderer';
 
-const NUM_ELEMENTS = 1000;
-const SPEED_MULTIPLIER = 0.1;
-const MS_PER_UPDATE = 1000 / 60;
+// const NUM_ELEMENTS = 1000;
+// const SPEED_MULTIPLIER = 0.1;
+// const MS_PER_UPDATE = 1000 / 60;
 
-// Initialize canvas
-let canvas = document.querySelector('canvas');
-let canvasWidth = canvas.width = window.innerWidth;
-let canvasHeight = canvas.height = window.innerHeight;
+// // Initialize canvas
+// let canvas = document.querySelector('canvas');
+// let canvasWidth = canvas.width = window.innerWidth;
+// let canvasHeight = canvas.height = window.innerHeight;
 
-window.addEventListener('resize', () => {
-  canvasWidth = canvas.width = window.innerWidth
-  canvasHeight = canvas.height = window.innerHeight;
-}, false);
+// window.addEventListener('resize', () => {
+//   canvasWidth = canvas.width = window.innerWidth
+//   canvasHeight = canvas.height = window.innerHeight;
+// }, false);
 
-// Create world and register the systems on it
-var world = new World();
-world
-  .registerSystem(Moveable)
-  .registerSystem(Renderer);
-world.getSystem(Renderer).stop();
+// // Create world and register the systems on it
+// var world = new World();
+// world
+//   .registerSystem(Moveable)
+//   .registerSystem(Renderer);
+// world.getSystem(Renderer).stop();
 
-// Some helper functions when creating the components
-function getRandomVelocity() {
-  return {
-    x: SPEED_MULTIPLIER * (2 * Math.random() - 1),
-    y: SPEED_MULTIPLIER * (2 * Math.random() - 1)
-  };
-}
+// // Some helper functions when creating the components
+// function getRandomVelocity() {
+//   return {
+//     x: SPEED_MULTIPLIER * (2 * Math.random() - 1),
+//     y: SPEED_MULTIPLIER * (2 * Math.random() - 1)
+//   };
+// }
 
-function getRandomPosition() {
-  return {
-    x: Math.random() * canvasWidth,
-    y: Math.random() * canvasHeight
-  };
-}
+// function getRandomPosition() {
+//   return {
+//     x: Math.random() * canvasWidth,
+//     y: Math.random() * canvasHeight
+//   };
+// }
 
-function getRandomShape() {
-    return {
-      primitive: Math.random() >= 0.5 ? 'circle' : 'box'
-    };
-}
+// function getRandomShape() {
+//     return {
+//       primitive: Math.random() >= 0.5 ? 'circle' : 'box'
+//     };
+// }
 
-for (let i = 0; i < NUM_ELEMENTS; i++) {
-  world
-    .createEntity()
-    .addComponent(Velocity, getRandomVelocity())
-    .addComponent(Shape, getRandomShape())
-    .addComponent(Position, getRandomPosition())
-    .addComponent(Renderable)
-}
+// for (let i = 0; i < NUM_ELEMENTS; i++) {
+//   world
+//     .createEntity()
+//     .addComponent(Velocity, getRandomVelocity())
+//     .addComponent(Shape, getRandomShape())
+//     .addComponent(Position, getRandomPosition())
+//     .addComponent(Renderable)
+// }
 
-let lag = 0;
+// let lag = 0;
 
-// Run!
-function run() {
-  // Compute delta and elapsed time
-  var time = performance.now();
-  var delta = time - lastTime;
-  lag += delta;
+// // Run!
+// function run() {
+//   // Compute delta and elapsed time
+//   var time = performance.now();
+//   var delta = time - lastTime;
+//   lag += delta;
 
-  while (lag >= MS_PER_UPDATE) {
-    // Run all the systems
-    world.execute();
-    lag -= MS_PER_UPDATE;
+//   while (lag >= MS_PER_UPDATE) {
+//     // Run all the systems
+//     world.execute();
+//     lag -= MS_PER_UPDATE;
+//   }
+
+//   world.getSystem(Renderer).execute(delta, time, lag / MS_PER_UPDATE);
+
+//   lastTime = time;
+//   requestAnimationFrame(run);
+// }
+
+// var lastTime = performance.now();
+// run();
+
+import geckos from '@geckos.io/client';
+
+const channel = geckos({ port: process.env.PORT || 3000 });
+
+channel.onConnect((error: any) => {
+  if (error) {
+    console.error(error.message);
+    return;
   }
 
-  world.getSystem(Renderer).execute(delta, time, lag / MS_PER_UPDATE);
+  channel.onDisconnect(() => {
+    console.log('Disconnected from server');
+  });
 
-  lastTime = time;
-  requestAnimationFrame(run);
-}
+  channel.on('chat message', (data: any) => {
+    console.log(`received: ${data}`);
+  })
 
-var lastTime = performance.now();
-run();
+  channel.emit('chat message', 'a short message sent to the server');
+});
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,17 @@
-console.log('Hello from server');
+import geckos from '@geckos.io/server';
+
+const io = geckos();
+
+io.listen(parseInt(process.env.PORT || '3000'));
+
+io.onConnection(channel => {
+  channel.onDisconnect(() => {
+    console.log(`${channel.id} got disconnected`);
+  })
+
+  channel.on('chat message', (data: any) => {
+    console.log(`received: ${data}`);
+    // emit the "chat message" data to all channels in the same room
+    io.room(channel.roomId).emit('chat message', data);
+  })
+});
