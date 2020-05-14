80c5d9d2f66637906cda7673d2ec5f1b3aa46ec2
Nicky van Urk <nickyvanurk@gmx.com>
2020-05-14 22:48:48 +0200

    Add logger

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,17 +1,19 @@
 import geckos from '@geckos.io/server';
+import logger from './utils/logger';
 
 const io = geckos();
 
 io.listen(parseInt(process.env.PORT || '3000'));
 
 io.onConnection(channel => {
+  logger.info(`Client connected`);
+
   channel.onDisconnect(() => {
-    console.log(`${channel.id} got disconnected`);
+    logger.info(`Client disonnected`);
   })
 
   channel.on('chat message', (data: any) => {
-    console.log(`received: ${data}`);
-    // emit the "chat message" data to all channels in the same room
+    logger.info(`Client message: ${data}`);
     io.room(channel.roomId).emit('chat message', data);
   })
 });
---
+++ server/src/utils/logger.ts
@@ -0,0 +1,15 @@
+import {createLogger, format, transports} from 'winston';
+
+const logger = createLogger({
+  format: format.combine(
+    format.colorize(),
+    format.simple()
+  ),
+  transports: [
+    new transports.Console()
+  ]
+});
+
+logger.level = process.env.LOG_LEVEL || 'debug';
+
+export default logger;
