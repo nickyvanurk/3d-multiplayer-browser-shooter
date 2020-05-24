7a9433392fd87f52283c25affd4105e7c4e7f275
Nicky van Urk <nickyvanurk@gmx.com>
2020-05-25 01:46:25 +0200

    Remove unused member variable

--- client/src/components/render-pass.ts
+++ client/src/components/render-pass.ts
@@ -3,7 +3,6 @@ import {Scene as Scene$1} from 'three';
 
 export class RenderPass extends Component {
   public scene: Scene$1;
-  public camera: Entity;
 
   constructor() {
     super();
@@ -13,11 +12,9 @@ export class RenderPass extends Component {
 
   copy(src: RenderPass) {
     this.scene = src.scene;
-    this.camera = src.camera;
   }
 
   reset() {
     this.scene = null;
-    this.camera = null;
   }
 }
