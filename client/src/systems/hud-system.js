8010e8f10d332b914caf8ca6b4983255e09d9d1d
Nicky van Urk <nickyvanurk@gmx.com>
2021-03-15 02:21:19 +0100

    Correctly position the HUD enemy indicators

--- client/src/systems/hud-system.js
+++ client/src/systems/hud-system.js
@@ -1,11 +1,13 @@
-import { System, Not } from 'ecsy';
+import { System } from 'ecsy';
 import { TextureLoader, SpriteMaterial, Sprite, OrthographicCamera, Scene } from 'three';
+import { Vector3, Object3D } from 'three';
 
 import Types from '../../../shared/types';
 import { Transform } from '../components/transform';
 import { Kind } from '../../../shared/components/kind';
 import { Player } from '../components/player';
 import { WebGlRenderer } from '../components/webgl-renderer';
+import { Object3d } from '../components/object3d';
 
 export class HudSystem extends System {
   static queries = {
@@ -73,20 +75,39 @@ export class HudSystem extends System {
         const player = this.queries.player.results[0];
         if (!player) return;
 
-        const { position: playerPosition } = player.getComponent(Transform);
+        const renderer = this.queries.renderers.results[0];
+        const camera = renderer.getComponent(WebGlRenderer).camera;
+        const {
+          position: cameraPosition,
+          quaternion: cameraRotation
+        } = camera.getComponent(Object3d).value;
+
         const { position } = entity.getComponent(Transform);
 
-        const relativePosition = {
-          x: playerPosition.x - position.x,
-          y: playerPosition.y - position.y
-        };
+        let relativePosition = new Vector3();
+        relativePosition.subVectors(cameraPosition, position);
 
-        const angle = Math.atan2(relativePosition.y, relativePosition.x);
-        const x = this.width/3 * Math.cos(angle);
-        const y = this.height/3 * Math.sin(angle);
+        const dummy = new Object3D();
+        dummy.quaternion.copy(cameraRotation);
+        dummy.updateMatrixWorld();
+        const localPosition = dummy.worldToLocal(relativePosition).normalize();
 
+        const dotX = new Vector3(0, 0, 1)
+          .dot(new Vector3(localPosition.x, 0, localPosition.z)
+          .normalize());
+        const dotY = new Vector3(0, 0, 1)
+          .dot(new Vector3(0, localPosition.y, localPosition.z)
+          .normalize());
+
+        // TODO: Correct elliptical x,y
         const indicator = this.entityIndicators[entity.id];
+        const angle = Math.atan2(localPosition.y, localPosition.x);
+        const radius = (this.width < this.height ? this.width : this.height)/3;
+        const x = radius * Math.cos(angle);
+        const y = radius * Math.sin(angle);
+
         indicator.position.set(-x, -y, 1);
+        indicator.visible = dotX < 0.9 || dotY < 0.9;
       }
     });
   }
